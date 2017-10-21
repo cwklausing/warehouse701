4adbde6c7584c519b47e88aa778426bf2e7288c0
cwklausing <cwklausing@gmail.com>
2017-10-20 21:38:21 -0500

    parallax home-header + transparent about section

--- parts/front-page-about.php
+++ parts/front-page-about.php
@@ -8,14 +8,16 @@ $about_button_url = get_field( 'about_button_url' );
 $about_text   = get_field( 'about_text' );
 ?>
 
-<section class="home-about content-width">
-  <div class="home-about__left">
-    <h2 class="home-about__title"><?php echo esc_html($about_header); ?></h2>
-    <a href="<?php echo esc_url( $about_button_url ); ?>" class="button">
-      <?php echo esc_html( $about_button_text ); ?>
-    </a>
-  </div>
-  <div class="home-about__right">
-    <p class="home-about__text"><?php echo esc_html($about_text); ?></p>
+<section class="home-about">
+  <div class="home-about__content content-width">
+    <div class="home-about__left">
+      <h2 class="home-about__title"><?php echo esc_html($about_header); ?></h2>
+      <a href="<?php echo esc_url( $about_button_url ); ?>" class="button">
+        <?php echo esc_html( $about_button_text ); ?>
+      </a>
+    </div>
+    <div class="home-about__right">
+      <p class="home-about__text"><?php echo esc_html($about_text); ?></p>
+    </div>
   </div>
 </section>
--- parts/front-page-circles.php
+++ parts/front-page-circles.php
@@ -7,12 +7,14 @@ $circles = get_field( 'circles' );
 ?>
 
 <div class="circles-section content-width">
-  <?php foreach( $circles as $circle ) : 
-    $text = $circle['text'];
-    $link = $circle['link'];
-    ?>
-    <a href="<?php echo esc_url( $link ); ?>" class="home-circle">
-      <?php echo esc_html( $text ); ?>
-    </a>
-  <?php endforeach; ?>
+  <div class="circles-section__content">
+    <?php foreach( $circles as $circle ) : 
+      $text = $circle['text'];
+      $link = $circle['link'];
+      ?>
+      <a href="<?php echo esc_url( $link ); ?>" class="home-circle">
+        <?php echo esc_html( $text ); ?>
+      </a>
+    <?php endforeach; ?>
+  </div>
 </div>
