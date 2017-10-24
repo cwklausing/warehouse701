9ff2533c35f564c7e695f74653b1aa72aee66caf
cwklausing <cwklausing@gmail.com>
2017-10-23 22:23:56 -0500

    beginning of spaces section. No slider

--- parts/front-page-spaces.php
+++ parts/front-page-spaces.php
@@ -2,30 +2,32 @@
 /**
 * Front page spaces
 */
-$title = get_field( 'featured_spaces_title' );
 $spaces = get_field( 'featured_spaces' );
+$button_text = get_field( 'spaces_button_text' );
 ?>
-
-<div class="home-spaces-section">
-  <h2 class="home-spaces-section__title content-width">
-    <?php echo esc_html( $title ); ?>
-  </h2>
-  <div class="home-spaces">
-    <?php foreach ( $spaces as $space ) :
-      $space_obj = $space['featured_space'];
-      $post = $space_obj;
-      setup_postdata($post);
-      $title = get_the_title();
-      $gallery = get_field( 'picture_gallery' );
-      $pic = $gallery[0]['sizes']['medium_large'];
-      ?>
-      <!-- To Do: Change background pic on hover - probably making two background divs within container -->
-      <div class="home-space" style="background-image:url('<?php echo esc_url( $pic ); ?>')">
-        <h3 class="home-space__name"><?php echo esc_html( $title ); ?></h3>
-        <a href="#" class="home-space__button button"><?php echo esc_html(); ?></a>
+<!-- TODO: Add Slick Slider for spaces -->
+<div class="home-spaces">
+  <?php foreach ( $spaces as $space ) :
+    $space_obj = $space['featured_space'];
+    $description = $space['custom_excerpt'];
+    $post = $space_obj;
+    setup_postdata($post);
+    $title = get_the_title();
+    $gallery = get_field( 'picture_gallery' );
+    $pic = $gallery[0]['sizes']['medium_large'];
+    ?>
+    <div class="home-space">
+      <div class="home-space__image" style="background-image:url('<?php echo esc_url( $pic ); ?>')">
+        <div class="home-space__overlay"></div>
+        <h3 class="home-space__title"><?php echo esc_html( $title ); ?></h3>
       </div>
-    <?php 
-    wp_reset_postdata();
-    endforeach; ?>
-  </div>
+      <div class="home-space__text">
+        <h4 class="home-space__title-two"><?php echo esc_html( $title ); ?></h4>
+        <p class="home-space__description"><?php echo esc_html( $description ); ?></p>
+        <a href="#" class="home-space__button button"><?php echo esc_html( $button_text ); ?></a>
+      </div>
+    </div>
+  <?php 
+  wp_reset_postdata();
+  endforeach; ?>
 </div>
